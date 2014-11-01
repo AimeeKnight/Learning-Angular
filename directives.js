c8f8755835b330eafa87c7373d6b11e95d22962e
AimeeKnight <aimee.knight@messagesystems.com>
2014-11-01 19:28:43 -0400

    add email validation directive

--- directives.js
+++ directives.js
@@ -28,3 +28,46 @@ app.directive('requiredFile',function() {
      }
    };
  });
+
+// directive for email validation
+ .directive('validEmail', function() {
+   return {
+     require: 'ngModel',
+     link: function(scope, elem, attrs, ctrl) {
+
+       var extractEmail = function (email) {
+         var bracketRegex = /(.+)<(.*)>$/
+           , bracketMatches = bracketRegex.exec(email);
+
+         // only extract email if it's wrapped in angle brackets, and it has a valid display name
+         if (typeof email === 'string' && bracketMatches !== null && bracketMatches[1].indexOf('@') === -1 && bracketMatches[1] !== ' ') {
+           email = bracketMatches[2];
+         }
+         return email.trim();
+       };
+
+       var isValidEmail = function(email) {
+         var EMAIL_REGEXP = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-z
+         return EMAIL_REGEXP.test(email);
+       };
+
+       ctrl.$parsers.push(function(value) {
+         if (ctrl.$isEmpty(value)) {
+           ctrl.$setValidity('validEmail', true);
+           return value;
+         }
+         var email = extractEmail(value);
+         if ((isValidEmail(email) === false || value.split(',').length > 1)) {
+           ctrl.$setValidity('validEmail', false);
+           return undefined;
+         } else {
+           ctrl.$setValidity('validEmail', true);
+           return value;
+         }
+       });
+     }
+   };
+ });
+});
+
+
